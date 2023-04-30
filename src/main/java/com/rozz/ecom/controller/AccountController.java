fa7f90a116c1a93877cf6ed9e5d7e7b44c0a1362
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-04-30 00:33:42 +0000

    set up controller for Account

---
+++ src/main/java/com/rozz/ecom/controller/AccountController.java
@@ -0,0 +1,31 @@
+package com.rozz.ecom.controller;
+
+import com.rozz.ecom.dto.AccountRequest;
+import com.rozz.ecom.dto.AccountResponse;
+import com.rozz.ecom.service.AccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/account")
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final AccountService accountService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody AccountRequest accountRequest) {
+        accountService.createAccount(accountRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<AccountResponse> getAllAccounts() {
+        accountService.getAllAccounts();
+    }
+
+}
