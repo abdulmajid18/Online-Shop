4ab22f4ec3f2aacc540495d79ff3cde493f3dbe8
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-04-30 01:30:54 +0000

    added getallproduct to service

--- src/main/java/com/rozz/ecom/service/AccountService.java
+++ src/main/java/com/rozz/ecom/service/AccountService.java
@@ -1,11 +1,15 @@
 package com.rozz.ecom.service;
 
 import com.rozz.ecom.dto.AccountRequest;
+import com.rozz.ecom.dto.AccountResponse;
 import com.rozz.ecom.entity.Account;
+import com.rozz.ecom.entity.AddressStatus;
 import com.rozz.ecom.reposiory.AccountRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,9 +24,23 @@ public class AccountService {
                 .lastName(accountRequest.getLastName())
                 .email(accountRequest.getEmail())
                 .phone(accountRequest.getPhone())
-                .password(accountRequest.getPassword()).build();
+                .password(accountRequest.getPassword())
+                .status(accountRequest.getDefaultStatus()).build();
         accountRepository.save(account);
         log.info("Account {} created and saved!", account.getUserName());
+    }
 
+    public List<AccountResponse> getAllAccounts() {
+        List<Account>  accounts = (List<Account>) accountRepository.findAll();
+        return accounts.stream().map(this::mapToAccountResponse).toList();
+    }
+
+    private AccountResponse mapToAccountResponse(Account account) {
+        return AccountResponse.builder()
+                .userName(account.getUserName())
+                .firsName(account.getFirsName())
+                .lastName(account.getLastName())
+                .phone(account.getPhone())
+                .email(account.getEmail()).build();
     }
 }
