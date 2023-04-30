7546ab12424d403824c019939ef98392aabef2d0
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-04-30 00:34:30 +0000

    Account Response data transfer object

---
+++ src/main/java/com/rozz/ecom/dto/AccountResponse.java
@@ -0,0 +1,22 @@
+package com.rozz.ecom.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountResponse {
+    private UUID id;
+    private String userName;
+    private String firsName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String password;
+}
