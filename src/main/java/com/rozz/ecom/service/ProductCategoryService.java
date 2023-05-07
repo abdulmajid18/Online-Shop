736336abf3be7c5819eb84156afa1d87f3eb5d7d
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-05-07 16:14:26 +0000

    added listall and create productcategory service

---
+++ src/main/java/com/rozz/ecom/service/ProductCategoryService.java
@@ -0,0 +1,41 @@
+package com.rozz.ecom.service;
+
+import com.rozz.ecom.dto.AccountResponse;
+import com.rozz.ecom.dto.ProductCategoryRequest;
+import com.rozz.ecom.dto.ProductCategoryResponse;
+import com.rozz.ecom.entity.Account;
+import com.rozz.ecom.entity.ProductCategory;
+import com.rozz.ecom.reposiory.ProductCategoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductCategoryService {
+    private ProductCategoryRepository productCategoryRepository;
+
+    public void createCategory(ProductCategoryRequest productCategoryRequest) {
+        ProductCategory productCategory = ProductCategory.builder()
+                .name(productCategoryRequest.getName())
+                .description(productCategoryRequest.getDescription())
+                .build();
+        productCategoryRepository.save(productCategory);
+        log.info("Product {} with description {} saved!", productCategory.getName(),productCategory.getDescription());
+    }
+
+    public List<ProductCategoryResponse> getAllAccounts() {
+        List<ProductCategory> productCategories = (List<ProductCategory>) productCategoryRepository.findAll();
+        return productCategories.stream().map(this::mapToProductCategoryResponse).toList();
+    }
+
+    private ProductCategoryResponse mapToProductCategoryResponse(ProductCategory productCategory) {
+        return ProductCategoryResponse.builder()
+                .id(productCategory.getId())
+                .name(productCategory.getName())
+                .description(productCategory.getDescription()).build();
+    }
+}
