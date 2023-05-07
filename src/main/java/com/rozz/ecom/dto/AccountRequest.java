3e17d1c25612bd15cb8c2f86fbd75f372960c488
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-05-07 13:45:02 +0000

    chnaged attributes names

--- src/main/java/com/rozz/ecom/dto/AccountRequest.java
+++ src/main/java/com/rozz/ecom/dto/AccountRequest.java
@@ -1,29 +1,25 @@
 package com.rozz.ecom.dto;
 
-import com.rozz.ecom.entity.Address;
-import com.rozz.ecom.entity.AddressStatus;
-import com.rozz.ecom.entity.Product;
-import jakarta.persistence.*;
+import com.rozz.ecom.entity.AccountStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.UUID;
 // DTO  data transfer objects
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccountRequest {
-    private String userName;
-    private String firsName;
-    private String lastName;
+    private String username;
+    private String firstname;
+    private String lastname;
     private String phone;
     private String email;
     private String password;
 
-    public AddressStatus getDefaultStatus() {
-        return AddressStatus.UNKNOWN;
+    public AccountStatus getDefaultStatus() {
+        return AccountStatus.UNKNOWN;
     }
 }
