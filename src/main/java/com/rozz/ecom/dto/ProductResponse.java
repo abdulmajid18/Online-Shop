edd03380ef1b998149347fa8f0d4f8c3e2bbe822
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-05-07 22:08:06 +0000

    updated ProductResponse dto

--- src/main/java/com/rozz/ecom/dto/ProductResponse.java
+++ src/main/java/com/rozz/ecom/dto/ProductResponse.java
@@ -8,12 +8,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductResponse {
+    private UUID id;
     private String name;
     private String description;
     private BigDecimal price;
