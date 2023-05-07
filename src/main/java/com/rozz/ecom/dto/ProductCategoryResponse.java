5de11bf969e4df120618b08e29f01fbeb8da6b3f
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-05-07 15:32:18 +0000

    product category dto

---
+++ src/main/java/com/rozz/ecom/dto/ProductCategoryResponse.java
@@ -0,0 +1,23 @@
+package com.rozz.ecom.dto;
+
+import com.rozz.ecom.entity.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCategoryResponse {
+    private UUID id;
+    private String name;
+    private String description;
+
+
+}
